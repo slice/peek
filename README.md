e99563d42224be38ffc53b52649aa0029ac0712a
slice <ryaneft@gmail.com>
2020-07-06 02:09:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# peek
+
+peek is an optionally dynamic mini-dashboard website for your servers.
+
+## Dynamic JSON
+
+You can expose a JSON endpoint that the website can fetch to present a link
+list, a meter display, or both.
+
+By default, the website is static. To enable dynamic JSON, edit `index.html`:
+
+```js
+window.DYNAMIC = true;
+window.DYNAMIC_JSON_ENDPOINT = "//dynamic.example.json";
+```
+
+`DYNAMIC_JSON_ENDPOINT` dictates the URL the website will `fetch`. Make sure
+that you have [CORS](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS)
+configured correctly if you'll have the website request something that isn't on
+the same origin (i.e. domain).
+
+If the endpoint starts with `//`, then the `//` will be replaced with the domain
+that the page is loaded at, even matching the protocol.
+
+### Example
+
+Here is a commentated example of the JSON you can return. Keep in mind that JSON
+doesn't actually support comments.
+
+```json
+{
+  // Link list. You can remove this entirely if you'd like.
+  "links": [
+    // A plain link.
+    { "url": "https://github.com" },
+    // A link with custom text.
+    { "url": "https://gitlab.com", "text": "GitLab" },
+    // A link with custom text that opens in a new tab.
+    { "url": "https://grafana.com", "text": "Grafana", "new_tab": true }
+  ],
+
+  // Meter display. Again, you can remove this entirely if you'd like.
+  "meters": [
+    // A meter. The minimum value is zero (this cannot be changed).
+    { "value": 25, "max": 50, "label": "Apples" },
+    // A meter with a unit.
+    { "value": 250, "max": 1000, "label": "Storage", "unit": "GB" },
+    // A meter that omits the maximum value. You can also hide the value
+    // altogether with `"showValue": false`.
+    { "value": 75, "label": "Bandwidth", "unit": "TB", "showMax": false }
+  ]
+}
+```
---
+++ assets/js/index.js
@@ -0,0 +1,108 @@
+const q = document.querySelector.bind(document);
+
+function h(tag, attributes = null, ...inner) {
+  const elem = document.createElement(tag);
+  if (attributes != null && attributes.length !== 0) {
+    for (const [key, value] of Object.entries(attributes)) {
+      if (value == null) {
+        continue;
+      }
+      elem.setAttribute(key, value);
+    }
+  }
+  for (const node of inner) {
+    if (typeof node === "string") {
+      elem.innerText = node;
+      break;
+    }
+    elem.appendChild(node);
+  }
+  return elem;
+}
+
+async function fetchDynamicJSON(endpoint = DYNAMIC_JSON_ENDPOINT) {
+  if (endpoint.startsWith("//")) {
+    endpoint = `${window.location.origin}/${endpoint.slice(2)}`;
+  }
+
+  let resp;
+  try {
+    resp = await fetch(endpoint);
+
+    if (!resp.ok) {
+      throw `Failed to fetch dynamic JSON: server responded with HTTP ${resp.status}.`;
+    }
+  } catch (error) {
+    throw `Failed to load dynamic JSON: ${error}`;
+  }
+
+  let object;
+  try {
+    object = await resp.json();
+  } catch (error) {
+    throw `Failed to parse dynamic JSON: ${error}`;
+  }
+  return object;
+}
+
+async function setupDynamicElements() {
+  const object = await fetchDynamicJSON();
+  let content = h("div", { class: "content" });
+
+  if (object.links != null) {
+    let links = h("ul");
+    for (const { url, text = url, new_tab = false } of object.links) {
+      let attribs = { href: url };
+      if (new_tab) {
+        attribs._target = "blank";
+      }
+      links.appendChild(h("li", null, h("a", attribs, text)));
+    }
+    content.appendChild(links);
+  }
+
+  if (object.meters != null) {
+    let meters = h("div", { class: "meters" });
+    for (const [
+      index,
+      {
+        max = 100,
+        value = 50,
+        unit = "",
+        label = "Meter",
+        showValue = true,
+        showMax = true,
+      },
+    ] of object.meters.entries()) {
+      const id = `generated-meter-${index}`;
+      const percentage = (value / max) * 100;
+      let fullLabel = showValue
+        ? `${label} (${value}${showMax ? `/${max}` : ""}${
+            unit === "" ? "" : ` ${unit}`
+          })`
+        : label;
+      let meter = h(
+        "div",
+        { class: "meter-container" },
+        h(
+          "div",
+          { class: "meter" },
+          h("div", { class: "meter-value", style: `width: ${percentage}%` })
+        ),
+        h("label", { for: id }, fullLabel)
+      );
+      meters.appendChild(meter);
+    }
+    content.appendChild(meters);
+  }
+
+  q("main").appendChild(content);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add(DYNAMIC ? "dynamic" : "static");
+
+  if (DYNAMIC) {
+    setupDynamicElements().catch(console.error);
+  }
+});
